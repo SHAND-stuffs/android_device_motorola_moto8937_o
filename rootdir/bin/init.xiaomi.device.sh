60f5e9db2819dbd8c1f2e1713834e7e5552d580a
me-cafebabe <me.cafebabe@gmail.com>
2023-07-31 18:19:43 +0800

    moto8937_o: rootdir: Add ahannah ACDB loading logic
    * From stock init.mmi.acdb.sh

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -9,6 +9,22 @@ set_acdb_path_props() {
 }
 
 case "$(cat /sys/motorola-msm8937-mach/variant)" in
+	"ahannah")
+		typeset -l carrier=$(getprop ro.carrier)
+
+		latam="timbr tefbr amxbr retbr"
+
+		if echo "${latam[@]}" | grep -w "$carrier" &>/dev/null; then
+			# use audio parameter specialized for latam market
+			set_acdb_path_props ahannah_bl
+		elif [ $carrier == "retid" ]; then
+			# use audio parameter specialized for indonesia
+			set_acdb_path_props ahannah_id
+		else
+			# use default audio parameter
+			set_acdb_path_props ahannah
+		fi
+		;;
 	*)
 		set_acdb_path_props "$(cat /sys/motorola-msm8937-mach/variant)"
 		;;
