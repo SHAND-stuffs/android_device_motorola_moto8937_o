e765c746f7ad85641f8ca34b3a0b6206ab57cb00
me-cafebabe <me.cafebabe@gmail.com>
2023-08-05 21:30:47 +0800

    moto8937_o: Add "moto8937_o_4_19" to OTA asserts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ DEVICE_PATH := device/motorola/moto8937_o
 USES_DEVICE_MOTOROLA_MOTO8937_O := true
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := moto8937_o,ahannah,aljeter,hannah,jeter,nora
+TARGET_OTA_ASSERT_DEVICE := moto8937_o,moto8937_o_4_19,ahannah,aljeter,hannah,jeter,nora
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
